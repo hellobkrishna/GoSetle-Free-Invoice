e45f7436ff1afed20a29cfa743f3b3df3fc24197
hellobkrishna <bablookumarsah333@gmail.com>
2022-05-29 07:58:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import InvoicePage from './components/InvoicePage'
+
+function App() {
+  return (
+    <div className="app">
+      
+      <h1 className="center fs-30">Gosetle<span style={{fontSize: '16px', position:"relative", top:-7}}>®</span>  Free Invoice</h1>
+      
+      
+      <InvoicePage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/initialData.ts
@@ -0,0 +1,87 @@
+import { ProductLine, Invoice } from './types'
+import getSymbolFromCurrency from 'currency-symbol-map'
+import axios from "axios";
+import React, {useEffect} from "react";
+
+
+
+
+// const getC=()=>{
+//   const baseURL = "http://ip-api.com/json?fields=status,country,currency";
+
+
+//   useEffect(() => {
+//     axios.get(baseURL)
+//     .then( res =>  {
+//       console.log("Country is : ", res);
+
+//    })
+//  },[])
+
+
+  
+// }
+
+
+
+export const initialProductLine: ProductLine = {
+  description: '',
+  quantity: '1',
+  rate: '0.00',
+}
+
+
+ 
+
+  
+
+
+
+
+
+export const initialInvoice: Invoice = {
+
+  
+  logo: '',
+  logoWidth: 100,
+  title: 'INVOICE',
+  companyName: '',
+  name: '',
+  companyAddress: '',
+  companyAddress2: '',
+  companyCountry: 'India',
+  billTo: 'Bill To:',
+  clientName: '',
+  clientAddress: '',
+  clientAddress2: '',
+  clientCountry: 'India',
+  invoiceTitleLabel: 'Invoice#',
+  invoiceTitle: '',
+  invoiceDateLabel: 'Invoice Date',
+  invoiceDate: '',
+  invoiceDueDateLabel: 'Due Date',
+  invoiceDueDate: '',
+  productLineDescription: 'Item Description',
+  productLineQuantity: 'Qty',
+  productLineQuantityRate: 'Rate',
+  productLineQuantityAmount: 'Amount',
+  productLines: [
+    {
+      description: 'GoSetle Premium Invoice ',
+      quantity: '1',
+      rate: '249.00',
+    },
+    // { ...initialProductLine },
+    // { ...initialProductLine },
+  ],
+  
+  subTotalLabel: 'Sub Total',
+  taxLabel: 'Sale Tax (10%)',
+  totalLabel: 'TOTAL',
+  currency: '',
+
+  notesLabel: 'Notes',
+  notes: 'Thank You, for your purchase. We’re so lucky to have customers like you! We hope your experience was awesome and we can’t wait to see you again soon.',
+  termLabel: 'Terms & Conditions',
+  term: 'Please make the payment by the due date.',
+}
